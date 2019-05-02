eae15838cd2f1b20474774d3fed15d391979bbd2
Thomas Willson <thomas.willson@me.com>
2019-05-01 21:25:36 -0700

    Add C++ compatibility.

--- include/udp_logging.h
+++ include/udp_logging.h
@@ -16,6 +16,10 @@
 #define UDP_LOGGING_MAX_PAYLOAD_LEN 2048
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "esp_system.h"
 #include "esp_log.h"
 
@@ -25,3 +29,7 @@ extern int udp_log_fd;
 int udp_logging_init(const char *ipaddr, unsigned long port, vprintf_like_t func);
 int udp_logging_vprintf( const char *str, va_list l );
 void udp_logging_free(va_list l);
+
+#ifdef __cplusplus
+}
+#endif
