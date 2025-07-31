9ec0a950ab5a0c87cdb52283e289c0ddc940a70d
volkmarnissen <volkmar.nissen@gmail.com>
2025-07-31 16:47:47 +0200

    udp_logging_free returns immediately  in case on uninitialized udp socket

--- udp_logging.c
+++ udp_logging.c
@@ -35,9 +35,10 @@ int get_socket_error_code(int socket)
 {
 	int result;
 	u32_t optlen = sizeof(int);
-	if(getsockopt(socket, SOL_SOCKET, SO_ERROR, &result, &optlen) == -1) {
-	printf("getsockopt failed");
-	return -1;
+	if (getsockopt(socket, SOL_SOCKET, SO_ERROR, &result, &optlen) == -1)
+	{
+		printf("getsockopt failed");
+		return -1;
 	}
 	return result;
 }
@@ -49,33 +50,39 @@ int show_socket_error_reason(int socket)
 	return err;
 }
 
-void udp_logging_free(va_list l) {
-	int err = 0;
-	char *err_buf;
-    esp_log_set_vprintf(vprintf);
-    if( (err = shutdown(udp_log_fd, 2)) == 0 )
+void udp_logging_free(va_list l)
+{
+	if (udp_log_fd != 0)
 	{
-		vprintf("\nUDP socket shutdown!", l);
-	}else
-	{
-    	asprintf(&err_buf, "\nShutting-down UDP socket failed: %d!\n", err);
-		vprintf(err_buf, l);
-	}
+		int err = 0;
+		char *err_buf;
+		esp_log_set_vprintf(vprintf);
+		if ((err = shutdown(udp_log_fd, 2)) == 0)
+		{
+			vprintf("\nUDP socket shutdown!", l);
+		}
+		else
+		{
+			asprintf(&err_buf, "\nShutting-down UDP socket failed: %d!\n", err);
+			vprintf(err_buf, l);
+		}
 
-    if( (err = close( udp_log_fd )) == 0 )
-    {
-		vprintf("\nUDP socket closed!", l);
-	}else
-	{
-		asprintf(&err_buf, "\n Closing UDP socket failed: %d!\n", err);
-		vprintf(err_buf, l);
+		if ((err = close(udp_log_fd)) == 0)
+		{
+			vprintf("\nUDP socket closed!", l);
+		}
+		else
+		{
+			asprintf(&err_buf, "\n Closing UDP socket failed: %d!\n", err);
+			vprintf(err_buf, l);
+		}
+		udp_log_fd = 0;
 	}
-    udp_log_fd = 0;
 }
 
-
-int udp_logging_vprintf( const char *str, va_list l ) {
-    int err = 0;
+int udp_logging_vprintf(const char *str, va_list l)
+{
+	int err = 0;
 	int len;
 	char task_name[16];
 	char *cur_task = pcTaskGetName(xTaskGetCurrentTaskHandle());
@@ -83,8 +90,8 @@ int udp_logging_vprintf( const char *str, va_list l ) {
 	task_name[15] = 0;
 	if (strncmp(task_name, "tiT", 16) != 0)
 	{
-		len = vsprintf((char*)buf, str, l);
-		if( (err = sendto(udp_log_fd, buf, len, 0, (struct sockaddr *)&serveraddr, sizeof(serveraddr))) < 0 )
+		len = vsprintf((char *)buf, str, l);
+		if ((err = sendto(udp_log_fd, buf, len, 0, (struct sockaddr *)&serveraddr, sizeof(serveraddr))) < 0)
 		{
 			show_socket_error_reason(udp_log_fd);
 			vprintf("\nFreeing UDP Logging. sendto failed!\n", l);
@@ -92,7 +99,7 @@ int udp_logging_vprintf( const char *str, va_list l ) {
 			return vprintf("UDP Logging freed!\n\n", l);
 		}
 	}
-	return vprintf( str, l );
+	return vprintf(str, l);
 }
 
 int udp_logging_init(const char *node, const char *service, vprintf_like_t func)
@@ -122,14 +129,13 @@ int udp_logging_init(const char *node, const char *service, vprintf_like_t func)
 
 	ESP_LOGI(TAG, "Logging to %s", node);
 
-    int err = setsockopt(udp_log_fd, SOL_SOCKET, SO_SNDTIMEO, (const char *)&send_timeout, sizeof(send_timeout));
-	if (err < 0) {
-	   ESP_LOGE("UDP_LOGGING", "Failed to set SO_SNDTIMEO. Error %d", err);
+	int err = setsockopt(udp_log_fd, SOL_SOCKET, SO_SNDTIMEO, (const char *)&send_timeout, sizeof(send_timeout));
+	if (err < 0)
+	{
+		ESP_LOGE("UDP_LOGGING", "Failed to set SO_SNDTIMEO. Error %d", err);
 	}
 
-    esp_log_set_vprintf(func);
+	esp_log_set_vprintf(func);
 
-    return 0;
+	return 0;
 }
-
-
